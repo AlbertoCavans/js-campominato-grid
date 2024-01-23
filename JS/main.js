b8763885083d6d0e2ac1a4649ce68ca87c03be3f
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-23 18:23:43 +0100

    Feat JS - clicks

--- JS/main.js
+++ JS/main.js
@@ -3,18 +3,27 @@ const gameBoard = document.getElementById("game-board");
 const startButton = document.querySelector(".btn");
 generateGameBoard(gameBoard);
 
+startButton.addEventListener("click", function () {
+  gameBoard.innerHTML = "";
+  generateGameBoard(gameBoard);
+});
+
 /* GENERATE GAMEBOARD */
 function generateGameBoard(container) {
-  for (let i = 0; i < 100; i++) {
-    const cell = generateCell();
+  for (let i = 1; i < 100 + 1; i++) {
+    const cell = generateCell(i);
     container.append(cell);
   }
 }
 
 /* GENERATE CELL */
-function generateCell() {
+function generateCell(text) {
   const singleCell = document.createElement("div");
   singleCell.classList.add("box");
+  singleCell.innerText = text;
+  singleCell.addEventListener("click", function () {
+    this.classList.toggle("clicked");
+  });
 
   return singleCell;
 }
