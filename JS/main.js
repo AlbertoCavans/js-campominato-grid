a9a8aeae850c4d82e72db34491bcb99d1c6eadea
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-24 13:32:17 +0100

    Feat JS - difficulty levels

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,29 @@
 /* LINKS TO ELEMENTS */
 const gameBoard = document.getElementById("game-board");
 const startButton = document.querySelector(".btn");
+const inputLevel = document.getElementById("inputLevel");
+
 generateGameBoard(gameBoard);
 
 startButton.addEventListener("click", function () {
-  gameBoard.innerHTML = "";
   generateGameBoard(gameBoard);
 });
 
 /* GENERATE GAMEBOARD */
 function generateGameBoard(container) {
-  for (let i = 1; i < 100 + 1; i++) {
-    const cell = generateCell(i);
+  gameBoard.innerHTML = "";
+  const numberCell = parseInt(inputLevel.value);
+  for (let i = 1; i < numberCell + 1; i++) {
+    const cell = generateCell(i, numberCell);
     container.append(cell);
   }
 }
 
 /* GENERATE CELL */
-function generateCell(text) {
+function generateCell(text, cellNumber) {
   const singleCell = document.createElement("div");
   singleCell.classList.add("box");
+  singleCell.classList.add("box-" + cellNumber);
   singleCell.innerText = text;
   singleCell.addEventListener("click", function () {
     this.classList.toggle("clicked");
