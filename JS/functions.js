917bdb8bf6c155dba004c38606af1451de6f23bb
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-24 18:23:02 +0100

    FIx JS - fix NaN

--- JS/functions.js
+++ JS/functions.js
@@ -3,9 +3,11 @@
 function getrandomNumber(min, max) {
   min = parseInt(min);
   max = parseInt(max);
-  if (isNan(min) || isNaN(max)) {
+  console.log(isNaN(max));
+
+  if (isNaN(min) || isNaN(max)) {
     console.error("I valori inseriti devono essere numerici");
-    return;
+    return false;
   }
   if (min >= max) {
     console.error("Il valore massimo deve essere maggiore del valore minimo");
